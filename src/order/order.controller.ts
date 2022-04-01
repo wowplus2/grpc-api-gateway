d64bb4bf29cf0f773668b15851bdd9eaaa61599e
wowplus <wowplus2@gmail.com>
2022-04-01 19:46:30 +0900

    api gateway 작업

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  OnModuleInit,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { AuthGuard } from 'src/auth/auth.guard';
+import {
+  CreateOrderRequest,
+  CreateOrderResponse,
+  OrderServiceClient,
+  ORDER_SERVICE_NAME,
+} from './order.pb';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController implements OnModuleInit {
+  private svc: OrderServiceClient;
+
+  @Inject(ORDER_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createOrder(
+    @Req() req: Request,
+  ): Promise<Observable<CreateOrderResponse>> {
+    const body: CreateOrderRequest = req.body;
+
+    // body.userId = <number>req.user;
+    body.userId = <number>req.body.user;
+
+    return this.svc.createOrder(body);
+  }
+}
